2d2a28ac3ab0a1bec4467ab86393656ab00d17cb
Fithen3 <120141318+Fithen3@users.noreply.github.com>
2026-08-11 12:50:15 +0300

    11.08.26

--- input-controller.js
+++ input-controller.js
@@ -1,13 +1,13 @@
 (function (global) {
     'use strict';
 
-    class InputController{
-        constructor(actionsToBind, target, plugins){
+    class InputController {
+        constructor(actionsToBind, target, plugins) {
             this._enabled = false;
-            this.focused = document.hasFocus(target);
+            this.focused = hasFocus(target);
             this.ACTION_ACTIVATED = 'input-controller:action-activated';
             this.ACTION_DEACTIVATED = 'input-controller:action-deactivated';
-           
+
             this._target = null;
             this._window = null;
             this._actions = new Map();
@@ -20,74 +20,82 @@
 
             (plugins || []).forEach((plugin) => this.addPlugin(plugin));
 
-            if (actionsToBind){
+            if (actionsToBind) {
                 this.bindActions(actionsToBind);
             }
-            if(target){
+            if (target) {
                 this.attach(target);
             }
         }
 
-        get enabled(){
-            return
+        get enabled() {
+            return this._enabled;
         }
-        set enabled(value){
+
+        set enabled(value) {
             this._enabled = Boolean(value);
             this._refreshActionStates(false);
         }
 
-        bindActions(actionsToBind){
-            if(!actionsToBind || typeof actionsToBind !== 'object'){
+        bindActions(actionsToBind) {
+            if (!actionsToBind || typeof actionsToBind !== 'object') {
                 return;
             }
 
             Object.keys(actionsToBind).forEach((actionName) => {
                 const settings = actionsToBind[actionName];
-                if(!settings || typeof settings !== 'object'){
+                if (!settings || typeof settings !== 'object') {
                     return;
                 }
 
-                const oldSettings = this._actions.get(actionName) || {enabled: true};
+                const oldSettings = this._actions.get(actionName) || { enabled: true };
                 this._actions.set(
                     actionName,
-                    Object.assign({}, oldSettings,settings)
+                    Object.assign({}, oldSettings, settings)
                 );
             });
 
             this._refreshActionStates(false);
         }
 
-        addPlugin(plugin){
-            if(!isPlagin(plugin)){
+        addPlugin(plugin) {
+            if (!isPlugin(plugin)) {
                 throw new TypeError('Некорректный плагин ввода');
             }
 
             const oldPlugin = this._plugins.get(plugin.name);
-            if(oldPlugin === plugin){
+            if (oldPlugin === plugin) {
                 return;
             }
-
-            if(oldPlugin){
+            if (oldPlugin) {
                 oldPlugin.detach();
             }
 
             this._plugins.set(plugin.name, plugin);
-            if(this._target){
+            if (this._target) {
                 plugin.attach(this._target, this._onPluginChange);
             }
             this._refreshActionStates(false);
         }
 
-        enableAction(actionName){
+        enableAction(actionName) {
             const action = this._actions.get(actionName);
-            if(action){
+            if (action) {
                 action.enabled = true;
                 this._refreshActionStates(false);
             }
         }
 
-        attach(target, dontEnable){
-            if(!target || typeof target.addEventListener !== 'function'){
+        disableAction(actionName) {
+            const action = this._actions.get(actionName);
+            if (action) {
+                action.enabled = false;
+                this._refreshActionStates(false);
+            }
+        }
+
+        attach(target, dontEnable) {
+            if (!target || typeof target.addEventListener !== 'function') {
                 return;
             }
 
@@ -96,7 +104,7 @@
             this._window = getWindow(target);
             this.focused = hasFocus(target);
 
-            if(this.window){
+            if (this._window) {
                 this._window.addEventListener('blur', this._onWindowBlur);
                 this._window.addEventListener('focus', this._onWindowFocus);
             }
@@ -104,12 +112,12 @@
                 plugin.attach(target, this._onPluginChange);
             });
 
-            if(dontEnable !== true){
+            if (dontEnable !== true) {
                 this.enabled = true;
             }
         }
 
-        detach(){
+        detach() {
             this.enabled = false;
             this._removeListeners();
             this._target = null;
@@ -117,38 +125,41 @@
             this.focused = false;
         }
 
-        isActionActive(actionName){
+        isActionActive(actionName) {
             const action = this._actions.get(actionName);
-            if(!this.enabled || !this.focused || !action || action.enabled === false){
+            if (!this.enabled || !this.focused || !action || action.enabled === false) {
                 return false;
             }
 
-            for (const plugin of this._plugins.values()){
-                if(plugin.isActionActive(action, actionName)){
+            for (const plugin of this._plugins.values()) {
+                if (plugin.isActionActive(action, actionName)) {
                     return true;
                 }
             }
             return false;
         }
 
-        isKeyPressed(keyCode){
+        isKeyPressed(keyCode) {
             const keyboard = this._plugins.get('keyboard');
             return Boolean(
-                this.focused && keyboard &&  typeof keyboard.isKeyPressed === 'function' && keyboard.isKeyPressed(keyCode)
-            );               
+                this.focused &&
+                keyboard &&
+                typeof keyboard.isKeyPressed === 'function' &&
+                keyboard.isKeyPressed(keyCode)
+            );
         }
 
-        _onPluginChange(){
-            this._refreshActionStates(true);         
+        _onPluginChange() {
+            this._refreshActionStates(true);
         }
 
-        _refreshActionStates(shouldDispatch){
+        _refreshActionStates(shouldDispatch) {
             this._actions.forEach((action, actionName) => {
                 const wasActive = this._actionStates.get(actionName) === true;
                 const isActive = this.isActionActive(actionName);
                 this._actionStates.set(actionName, isActive);
 
-                if(!shouldDispatch || wasActive === isActive){
+                if (!shouldDispatch || wasActive === isActive) {
                     return;
                 }
                 this._dispatchActionEvent(
@@ -158,43 +169,43 @@
             });
         }
 
-        _dispatchActionEvent(eventName, actionName){
-            if(!this._target || !this.enabled || !this.focused){
+        _dispatchActionEvent(eventName, actionName) {
+            if (!this._target || !this.enabled || !this.focused) {
                 return;
             }
 
             const ownerWindow = getWindow(this._target);
             const CustomEventClass = (ownerWindow && ownerWindow.CustomEvent) || global.CustomEvent;
-            this._target._dispatchEvent(new CustomEventClass(eventName, {
-                datail: actionName
+            this._target.dispatchEvent(new CustomEventClass(eventName, {
+                detail: actionName
             }));
         }
 
-        _onWindowBlur(){
+        _onWindowBlur() {
             this.focused = false;
             this._plugins.forEach((plugin) => {
-                if(typeof plugin.reset === 'function'){
+                if (typeof plugin.reset === 'function') {
                     plugin.reset();
                 }
             });
             this._refreshActionStates(false);
         }
 
-        _onWindowFocus(){
+        _onWindowFocus() {
             this.focused = true;
             this._refreshActionStates(false);
         }
 
-        _removeListeners(){
-            if(this._window){
-                this._window._removeEventListeners('blur', this._onWindowBlur);
-                this._window._removeEventListeners('focus', this._onWindowFocus);
+        _removeListeners() {
+            if (this._window) {
+                this._window.removeEventListener('blur', this._onWindowBlur);
+                this._window.removeEventListener('focus', this._onWindowFocus);
             }
             this._plugins.forEach((plugin) => plugin.detach());
         }
     }
 
-    function isPlagin(plugin){
+    function isPlugin(plugin) {
         return plugin &&
             typeof plugin.name === 'string' &&
             typeof plugin.attach === 'function' &&
@@ -202,19 +213,24 @@
             typeof plugin.isActionActive === 'function';
     }
 
-
-    function getWindow(target){
-        return target && target.ownerDocument ? target.ownerDocument.defaultView : global;
+    function getWindow(target) {
+        return target && target.ownerDocument
+            ? target.ownerDocument.defaultView
+            : global;
     }
 
-    function hasFocus(target){
-        const documentObject = target && target.ownerDocument ? target.ownerDocument : global.document;
-        return !documentObject || typeof documentObject.hasFocus !== 'function' ? true : documentObject.hasFocus();
+    function hasFocus(target) {
+        const documentObject = target && target.ownerDocument
+            ? target.ownerDocument
+            : global.document;
+        return !documentObject || typeof documentObject.hasFocus !== 'function'
+            ? true
+            : documentObject.hasFocus();
     }
 
     global.InputController = InputController;
 
-    if(typeof module !== 'undefined' && module.exports){
+    if (typeof module !== 'undefined' && module.exports) {
         module.exports = InputController;
     }
-})(typeof window!== 'undefined' ? window : globalThis);
+})(typeof window !== 'undefined' ? window : globalThis);
